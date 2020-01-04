8e7751745899976652bdb7f900d912e2eb92e1c1
Masaharu Inoue <pasteur1822@gmail.com>
2020-01-05 01:28:33 +0900

    init

---
+++ src/called.js
@@ -0,0 +1,8 @@
+export default class Example{
+    constructor() {
+        return this
+    }
+    get(){
+        return "Hello"
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import Example from "./called.js"
+const example = new Example
+console.log(example.get())
